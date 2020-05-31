7b910289e666583e293a4233f179a3f0a44e682f
Thiago Sartor <tt.sartor@hotmail.com>
2020-05-31 20:38:18 -0300

    Adicionando a solução no repositório.

---
+++ Mercado.WinApp/GerenciarProdutoForm.Designer.cs
@@ -0,0 +1,359 @@
+﻿namespace Mercado.WinApp
+{
+    partial class GerenciarProdutoForm
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listaBusca = new System.Windows.Forms.ListBox();
+            this.btnBuscarTodos = new System.Windows.Forms.Button();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbCategoria = new System.Windows.Forms.ComboBox();
+            this.txtPreco = new System.Windows.Forms.MaskedTextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.txtNomeBusca = new System.Windows.Forms.TextBox();
+            this.listaBuscaPorNome = new System.Windows.Forms.ListBox();
+            this.btnBuscarPorNome = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbProduto = new System.Windows.Forms.Label();
+            this.numQntEstoque = new System.Windows.Forms.NumericUpDown();
+            this.btnBuscarId = new System.Windows.Forms.Button();
+            this.btnAtualizar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numQntEstoque)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listaBusca
+            // 
+            this.listaBusca.FormattingEnabled = true;
+            this.listaBusca.ItemHeight = 16;
+            this.listaBusca.Location = new System.Drawing.Point(20, 57);
+            this.listaBusca.Name = "listaBusca";
+            this.listaBusca.Size = new System.Drawing.Size(262, 132);
+            this.listaBusca.TabIndex = 0;
+            // 
+            // btnBuscarTodos
+            // 
+            this.btnBuscarTodos.Location = new System.Drawing.Point(186, 25);
+            this.btnBuscarTodos.Name = "btnBuscarTodos";
+            this.btnBuscarTodos.Size = new System.Drawing.Size(96, 26);
+            this.btnBuscarTodos.TabIndex = 1;
+            this.btnBuscarTodos.Text = "Buscar";
+            this.btnBuscarTodos.UseVisualStyleBackColor = true;
+            this.btnBuscarTodos.Click += new System.EventHandler(this.btnBuscarTodos_Click);
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(183, 153);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(96, 26);
+            this.btnSalvar.TabIndex = 4;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.cmbCategoria);
+            this.groupBox1.Controls.Add(this.txtPreco);
+            this.groupBox1.Controls.Add(this.txtNome);
+            this.groupBox1.Controls.Add(this.btnSalvar);
+            this.groupBox1.Controls.Add(this.btnLimpar);
+            this.groupBox1.Location = new System.Drawing.Point(14, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(302, 204);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Adicionar novo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 17);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Categoria:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 17);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Preço:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 17);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Nome:";
+            // 
+            // cmbCategoria
+            // 
+            this.cmbCategoria.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbCategoria.FormattingEnabled = true;
+            this.cmbCategoria.Items.AddRange(new object[] {
+            "Frios",
+            "Carne",
+            "Higiene",
+            "Informárica",
+            "Padaria",
+            "Bebida"});
+            this.cmbCategoria.Location = new System.Drawing.Point(158, 108);
+            this.cmbCategoria.Name = "cmbCategoria";
+            this.cmbCategoria.Size = new System.Drawing.Size(121, 24);
+            this.cmbCategoria.TabIndex = 8;
+            // 
+            // txtPreco
+            // 
+            this.txtPreco.Location = new System.Drawing.Point(158, 68);
+            this.txtPreco.Mask = "0000,00";
+            this.txtPreco.Name = "txtPreco";
+            this.txtPreco.Size = new System.Drawing.Size(121, 22);
+            this.txtPreco.TabIndex = 7;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(158, 29);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(121, 22);
+            this.txtNome.TabIndex = 6;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(81, 153);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(96, 26);
+            this.btnLimpar.TabIndex = 5;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listaBusca);
+            this.groupBox2.Controls.Add(this.btnBuscarTodos);
+            this.groupBox2.Location = new System.Drawing.Point(354, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(302, 204);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Buscar todos";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.txtNomeBusca);
+            this.groupBox3.Controls.Add(this.listaBuscaPorNome);
+            this.groupBox3.Controls.Add(this.btnBuscarPorNome);
+            this.groupBox3.Location = new System.Drawing.Point(354, 233);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(302, 204);
+            this.groupBox3.TabIndex = 9;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Filtrar por nome";
+            // 
+            // txtNomeBusca
+            // 
+            this.txtNomeBusca.Location = new System.Drawing.Point(20, 26);
+            this.txtNomeBusca.Name = "txtNomeBusca";
+            this.txtNomeBusca.Size = new System.Drawing.Size(160, 22);
+            this.txtNomeBusca.TabIndex = 2;
+            // 
+            // listaBuscaPorNome
+            // 
+            this.listaBuscaPorNome.FormattingEnabled = true;
+            this.listaBuscaPorNome.ItemHeight = 16;
+            this.listaBuscaPorNome.Location = new System.Drawing.Point(20, 57);
+            this.listaBuscaPorNome.Name = "listaBuscaPorNome";
+            this.listaBuscaPorNome.Size = new System.Drawing.Size(262, 132);
+            this.listaBuscaPorNome.TabIndex = 0;
+            // 
+            // btnBuscarPorNome
+            // 
+            this.btnBuscarPorNome.Location = new System.Drawing.Point(186, 25);
+            this.btnBuscarPorNome.Name = "btnBuscarPorNome";
+            this.btnBuscarPorNome.Size = new System.Drawing.Size(96, 26);
+            this.btnBuscarPorNome.TabIndex = 1;
+            this.btnBuscarPorNome.Text = "Buscar";
+            this.btnBuscarPorNome.UseVisualStyleBackColor = true;
+            this.btnBuscarPorNome.Click += new System.EventHandler(this.btnBuscarPorNome_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.btnAtualizar);
+            this.groupBox4.Controls.Add(this.label6);
+            this.groupBox4.Controls.Add(this.txtId);
+            this.groupBox4.Controls.Add(this.label5);
+            this.groupBox4.Controls.Add(this.lbProduto);
+            this.groupBox4.Controls.Add(this.numQntEstoque);
+            this.groupBox4.Controls.Add(this.btnBuscarId);
+            this.groupBox4.Location = new System.Drawing.Point(14, 233);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(302, 204);
+            this.groupBox4.TabIndex = 8;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Atualizar estoque";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(44, 59);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(23, 17);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Id:";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(71, 58);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(56, 22);
+            this.txtId.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(44, 159);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Quantode:";
+            // 
+            // lbProduto
+            // 
+            this.lbProduto.AutoSize = true;
+            this.lbProduto.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbProduto.Location = new System.Drawing.Point(43, 110);
+            this.lbProduto.Name = "lbProduto";
+            this.lbProduto.Size = new System.Drawing.Size(84, 20);
+            this.lbProduto.TabIndex = 7;
+            this.lbProduto.Text = "Produto1";
+            // 
+            // numQntEstoque
+            // 
+            this.numQntEstoque.Location = new System.Drawing.Point(125, 158);
+            this.numQntEstoque.Name = "numQntEstoque";
+            this.numQntEstoque.Size = new System.Drawing.Size(83, 22);
+            this.numQntEstoque.TabIndex = 6;
+            // 
+            // btnBuscarId
+            // 
+            this.btnBuscarId.Location = new System.Drawing.Point(135, 56);
+            this.btnBuscarId.Name = "btnBuscarId";
+            this.btnBuscarId.Size = new System.Drawing.Size(73, 26);
+            this.btnBuscarId.TabIndex = 4;
+            this.btnBuscarId.Text = "Buscar";
+            this.btnBuscarId.UseVisualStyleBackColor = true;
+            this.btnBuscarId.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnAtualizar
+            // 
+            this.btnAtualizar.Location = new System.Drawing.Point(223, 110);
+            this.btnAtualizar.Name = "btnAtualizar";
+            this.btnAtualizar.Size = new System.Drawing.Size(73, 26);
+            this.btnAtualizar.TabIndex = 14;
+            this.btnAtualizar.Text = "Atualizar";
+            this.btnAtualizar.UseVisualStyleBackColor = true;
+            this.btnAtualizar.Click += new System.EventHandler(this.btnAtualizar_Click);
+            // 
+            // GerenciarProdutoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(670, 449);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.Name = "GerenciarProdutoForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gerenciar Produto";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numQntEstoque)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listaBusca;
+        private System.Windows.Forms.Button btnBuscarTodos;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cmbCategoria;
+        private System.Windows.Forms.MaskedTextBox txtPreco;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox txtNomeBusca;
+        private System.Windows.Forms.ListBox listaBuscaPorNome;
+        private System.Windows.Forms.Button btnBuscarPorNome;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button btnBuscarId;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbProduto;
+        private System.Windows.Forms.NumericUpDown numQntEstoque;
+        private System.Windows.Forms.Button btnAtualizar;
+    }
+}
+
---
+++ Mercado.WinApp/GerenciarProdutoForm.cs
@@ -0,0 +1,63 @@
+﻿using Mercado.Dados;
+using Mercado.Dominio;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mercado.WinApp
+{
+    public partial class GerenciarProdutoForm : Form
+    {
+        public GerenciarProdutoForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBuscarTodos_Click(object sender, EventArgs e)
+        {
+            listaBusca.Items.Clear();
+
+        }
+
+        private void btnBuscarPorNome_Click(object sender, EventArgs e)
+        {
+            listaBuscaPorNome.Items.Clear();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            LimparCampos();
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            LimparCampos();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listaBuscaPorNome.Items.Clear();
+        }
+
+        private void btnAtualizar_Click(object sender, EventArgs e)
+        {
+            LimparCampos();
+        }
+
+        private void LimparCampos()
+        {
+            txtNome.Clear();
+            txtPreco.Clear();
+            cmbCategoria.SelectedIndex = -1;
+            txtId.Clear();
+            txtNomeBusca.Clear();
+            numQntEstoque.Value = 0;
+        }
+    }
+}
